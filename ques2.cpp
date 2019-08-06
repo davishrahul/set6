c43619a4e2eca22af8b34935c3c9e532158fdb6a
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-06 12:08:52 +0530

    Create ques2.cpp

---
+++ ques2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+        int number;
+        cin>>number;
+        switch(number)
+        {
+            case 1:
+            cout<<"one";
+            break;
+            case 2:
+            cout<<"two";
+            break;
+            case 3:
+            cout<<"three";
+            break;
+            case 4:
+            cout<<"four";
+            break;
+            case 5:
+            cout<<"five";
+            break;
+            case 6:
+            cout<<"six";
+            break;
+            case 7:
+            cout<<"seven";
+            break;
+            case 8:
+            cout<<"eight";
+            break;
+            case 9:
+            cout<<"nine";
+            break;
+            default:
+            cout<<"print no from 1 to 9";
+
+        }
+        return 0;
+    }
