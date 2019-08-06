0aac91ed8d0099faf3890beee2dabdfc825f09ee
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-06 12:16:14 +0530

    Create ques4.cpp

---
+++ ques4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+    {   
+        int number;
+        cin>>number;
+        if(number%2==0)
+        {
+            cout<<number;
+        }
+        else
+        {
+            cout<<number-1;
+        }
+        
+        return 0;
+    }
+
